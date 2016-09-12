ccf49d7402417b61f8f10097a70977a1cddc73e9
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-12 09:50:33 +0300

    Image-resizer and mp3-decoder parameters moved to config

--- flog/configs/conf.py
+++ flog/configs/conf.py
@@ -11,5 +11,12 @@ username = config.get('AUTH', 'USERNAME')
 password = config.get('AUTH', 'PASSWORD')
 debug = config.get('ETC', 'DEBUG')
 upload_folder = config.get('PATH', 'UPLOAD_FOLDER')
+log_name = config.get('LOG', 'NAME')
+log_file = config.get('LOG', 'FILE')
+log_level = config.get('LOG', 'LEVEL')
+log_format = config.get('LOG', 'FORMAT')
+image_width = config.get('SERVICE', 'IMAGE_WIDTH')
+watermark_path = config.get('SERVICE', 'WATERMARK_PATH')
+mp3_bitrate = config.get('SERVICE', 'MP3_BITRATE')
 
-allowed_extensions = set(['mp3','jpg','jpeg','gif','png'])
+allowed_extensions = ['mp3','jpg','jpeg','gif','png']
