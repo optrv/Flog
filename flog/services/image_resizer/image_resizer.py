cba9f79cd11714c85959c933368564b6a3f42632
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-12 09:48:20 +0300

    Logger added
    Image-resizer and mp3-decoder parameters moved to config

--- flog/services/image_resizer/image_resizer.py
+++ flog/services/image_resizer/image_resizer.py
@@ -1,13 +1,10 @@
 import os
 from PIL import Image
-from flog.configs.conf import upload_folder
-
-width = 500 # Width of resized image
-path_watermark = 'image/watermark/watermark.png' # Path to watermark
+from flog.configs.conf import upload_folder, image_width, watermark_path
 
 # Watermark adding with specified (or default) value of position on image
 def watermark(image_out):
-    water = Image.open(os.path.join(upload_folder, path_watermark))
+    water = Image.open(os.path.join(upload_folder, watermark_path))
     water = water.resize((image_out.size[0] // 4, int((image_out.size[0] // 4 * water.size[1]) // water.size[0])), Image.BILINEAR)
     layer = Image.new('RGBA', image_out.size, (0, 0, 0, 0))
     pos_water = ((image_out.size[0] - 10 - water.size[0], image_out.size[1] - 10 - water.size[1]))
@@ -16,6 +13,7 @@ def watermark(image_out):
     return image_out
 
 def image_resizer(image, filename, subfolder):
+    width = int(image_width)
     image_out = Image.open(image)
     if image_out.mode != 'RGBA':
         image_out = image_out.convert('RGBA')
--- flog/services/mp3_decoder/mp3_decoder.py
+++ flog/services/mp3_decoder/mp3_decoder.py
@@ -1,7 +1,7 @@
 import os
 from pydub import AudioSegment
-from flog.configs.conf import upload_folder
+from flog.configs.conf import upload_folder, mp3_bitrate
 
 def mp3_decoder(files, filename, subfolder):
     track = AudioSegment.from_mp3(files)
-    track.export(os.path.join(upload_folder, subfolder, files.filename), format="mp3", bitrate="128k")
+    track.export(os.path.join(upload_folder, subfolder, filename), format = "mp3", bitrate = mp3_bitrate)
