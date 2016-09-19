260d1bfe9f61514582c75aea994d6fbd946e463a
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-19 07:18:45 +0300

    'flask-paginate' added

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'FLOG',
-    version = '0.79',
-    description = 'FLOG: Flask Blog',
-    url = 'https://github.com/optrv/flog',
-    author = 'optrv',
-    author_email = 'petr.ov@icloud.com',
-    install_requires = ['Flask', 'Pillow', 'pydub'],
-    packages = find_packages()
+    name='FLOG',
+    version='0.79',
+    description='FLOG: Flask Blog',
+    url='https://github.com/optrv/flog',
+    author='optrv',
+    author_email='petr.ov@icloud.com',
+    install_requires=['Flask', 'flask-paginate', 'Pillow', 'pydub'],
+    packages=find_packages()
 )
 
