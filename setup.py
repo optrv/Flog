c5d2da9204a65fa46045e3e4801dc653d137ee58
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-25 00:18:57 +0300

    Update setup.py
    Fixed and mysql module added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/optrv/flog',
     author='optrv',
     author_email='petr.ov@icloud.com',
-    install_requires=['Flask', 'flask-paginate', 'Pillow', 'pydub'],
+    install_requires=['Flask>=0.11', 'flask-paginate>=0.4.5', 'Pillow>=3.3.1', 'pydub>=0.16.5', 'mysql>=0.0.1'],
     packages=find_packages()
 )
 
