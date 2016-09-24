c756eeb7467ccf77bcf47d4437b9e384eeaf7033
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-25 00:36:16 +0300

    MySQL connector added (SQLite is still here as additional db)
    Now MySQL is the main database, except cases when MySQL-server is down or 'force_sqlite = True'

--- flog/models/models.py
+++ flog/models/models.py
@@ -1,20 +1,33 @@
 import os
 import sqlite3
+import MySQLdb
+from MySQLdb import escape_string as esc
 from datetime import datetime
 from flask import g
 from flask_paginate import Pagination
-from flog.configs.conf import database, upload_folder, allowed_extensions, username, password, posts_per_page
+from flog.configs.conf import (DATABASE, UPLOAD_FOLDER, ALLOWED_EXTENSIONS,
+                               USERNAME, PASSWORD, POSTS_PER_PAGE, FORCE_SQLITE)
 from flog.services.image_resizer.image_resizer import image_resizer
 from flog.services.mp3_decoder.mp3_decoder import mp3_decoder
 from werkzeug.utils import secure_filename
 
-posts_per_page = int(posts_per_page)
+posts_per_page = int(POSTS_PER_PAGE)
+force_sqlite = FORCE_SQLITE
+
+def connect_mysql():
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user="root",
+                         passwd="root", db="posts", use_unicode=True, charset="utf8")
+        cur = db.cursor()
+        return db, cur
+    except Exception:
+        pass
 
 def connect_db():
     """
     Connect with a database and use the sqlite3.Row-object
     """
-    row = sqlite3.connect(database)
+    row = sqlite3.connect(DATABASE)
     row.row_factory = sqlite3.Row
     return row
 
@@ -23,7 +36,7 @@ def init_db():
     Create the new database by schema.sql
     """
     db = get_db()
-    with open(os.path.dirname(database) + '/schema.sql', mode = 'r') as s:
+    with open(os.path.dirname(DATABASE) + '/schema.sql', mode = 'r') as s:
         db.cursor().executescript(s.read())
     db.commit()
 
@@ -42,16 +55,22 @@ def check_db():
     """
     Check if db-file is exist
     """
-    if not os.path.exists(database):
+    if not os.path.exists(DATABASE):
         init_db()
 
 def get_from_db():
     """
     Get the data from DB
     """
-    check_db()
-    db = get_db()
-    cur = db.execute('SELECT date_time, title, text, filename, filesave FROM posts ORDER BY id DESC')
+    sql_query = 'SELECT date_time, title, text, filename, filesave FROM posts ORDER BY id DESC'
+    if not force_sqlite and connect_mysql():
+        db, cur = connect_mysql()
+        cur.execute(sql_query)
+    else:
+        check_db()
+        db = get_db()
+        cur = db.execute(sql_query)
+
     posts = cur.fetchall()
     db.close()
     return posts
@@ -62,9 +81,14 @@ def add_to_db(date_time, title, text, filename, filesave):
     """
     if date_time is None:
         date_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    db = get_db()
-    db.execute('INSERT INTO posts (date_time, title, text, filename, filesave) '
-               'VALUES (?, ?, ?, ?, ?)',[date_time, title, text, filename, filesave])
+    if not force_sqlite and connect_mysql():
+        db, cur = connect_mysql()
+        cur.execute('INSERT INTO posts (date_time, title, text, filename, filesave) '
+                    'VALUES (%s, %s, %s, %s, %s)', (date_time, title, text, filename, filesave))
+    else:
+        db = get_db()
+        db.execute('INSERT INTO posts (date_time, title, text, filename, filesave) VALUES (?, ?, ?, ?, ?)',
+                    [date_time, title, text, filename, filesave])
     db.commit()
     db.close()
 
@@ -72,7 +96,7 @@ def save_file(files):
     """
     Save the file
     """
-    if files.filename.rsplit('.', 1)[1] in allowed_extensions:
+    if files.filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS:
         date_time = datetime.now()
         filename = secure_filename(files.filename)
         hashname = date_time.strftime("%Y%m%d%H%M%S") + '.' + filename.rsplit('.', 1)[1]
@@ -92,7 +116,7 @@ def check_login(user_name, pass_word):
     """
     Check login/password
     """
-    if (user_name != username or pass_word != password):
+    if (user_name != USERNAME or pass_word != PASSWORD):
         return False
     else:
         return True
