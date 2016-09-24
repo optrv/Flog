6684c5133683ec5323408702e257b9c235b20044
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-25 00:38:43 +0300

    Edited in accordance to Configparser documentation

--- flog/services/mp3_decoder/mp3_decoder.py
+++ flog/services/mp3_decoder/mp3_decoder.py
@@ -1,7 +1,7 @@
 import os
 from pydub import AudioSegment
-from flog.configs.conf import upload_folder, mp3_bitrate
+from flog.configs.conf import UPLOAD_FOLDER, MP3_BITRATE
 
 def mp3_decoder(files, filename, subfolder):
     track = AudioSegment.from_mp3(files)
-    track.export(os.path.join(upload_folder, subfolder, filename), format = "mp3", bitrate = mp3_bitrate)
+    track.export(os.path.join(UPLOAD_FOLDER, subfolder, filename), format = "mp3", bitrate = MP3_BITRATE)
