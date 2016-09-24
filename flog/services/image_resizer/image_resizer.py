0069da8dd3b7d459ea3cbf0332289920a2e926ec
Oleksandr Petrov <petr.ov@icloud.com>
2016-09-25 00:37:55 +0300

    Edited in accordance to Configparser documentation

--- flog/services/image_resizer/image_resizer.py
+++ flog/services/image_resizer/image_resizer.py
@@ -1,10 +1,10 @@
 import os
 from PIL import Image
-from flog.configs.conf import upload_folder, image_width, watermark_path
+from flog.configs.conf import UPLOAD_FOLDER, IMAGE_WIDTH, WATERMARK_PATH
 
 # Watermark adding with specified (or default) value of position on image
 def watermark(image_out):
-    water = Image.open(os.path.join(upload_folder, watermark_path))
+    water = Image.open(os.path.join(UPLOAD_FOLDER, WATERMARK_PATH))
     water = water.resize((image_out.size[0] // 4, int((image_out.size[0] // 4 * water.size[1]) // water.size[0])), Image.BILINEAR)
     layer = Image.new('RGBA', image_out.size, (0, 0, 0, 0))
     pos_water = ((image_out.size[0] - 10 - water.size[0], image_out.size[1] - 10 - water.size[1]))
@@ -13,7 +13,7 @@ def watermark(image_out):
     return image_out
 
 def image_resizer(image, filename, subfolder):
-    width = int(image_width)
+    width = int(IMAGE_WIDTH)
     image_out = Image.open(image)
     if image_out.mode != 'RGBA':
         image_out = image_out.convert('RGBA')
@@ -21,4 +21,4 @@ def image_resizer(image, filename, subfolder):
         # Resize image proportional to specified width
         image_out = image_out.resize((width, int((width * image_out.size[1]) / image_out.size[0])), Image.BILINEAR)
     image_out = watermark(image_out)
-    image_out.save(os.path.join(upload_folder, subfolder, filename))
+    image_out.save(os.path.join(UPLOAD_FOLDER, subfolder, filename))
